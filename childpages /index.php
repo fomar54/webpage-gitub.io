424d606d6e603e035b21949924026c12698148e6
fomar54 <99281331+fomar54@users.noreply.github.com>
2022-02-25 21:45:45 +0300

    Create index.php

---
+++ childpages /index.php
@@ -0,0 +1,10 @@
+<html>
+
+<form  action="index.html" method="post">
+<label>Name: <br> <input type="text" name="name" </br>
+
+<label>Messsage:<br><textarea cols="3"rows="4"name="message"></textarea><label></br>
+</form>
+
+
+</html>
