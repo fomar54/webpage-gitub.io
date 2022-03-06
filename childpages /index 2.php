f25e6c6ad0dc456416c9854d502fc4791126bbf8
fomar54 <99281331+fomar54@users.noreply.github.com>
2022-03-06 15:01:22 +0300

    Update index 2.php

--- childpages /index 2.php
+++ childpages /index 2.php
@@ -1,44 +1,8 @@
 <html>
+<body>
 
-<form  action="" method="post">
-<label>Name: <br> <input type="text" name="name" </br>
+Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?>
 
-<label>Messsage:<br><textarea cols="3"rows="4"name="message"></textarea><label></br>
-
-<input type="submit" name="submit" value"post">.
-</form>
+</body>
 </html>
-<?php
-
-$name = $_POST["name"];
-$text = $_POST["message"];
-$post =$_POST["submit"];
-
-if($post)
-#WRITE DOWN COMMENTS#
-$write= fopen("com.txt","a+");
-fwrite($write,"<u><b>$name</b></u><br>$text"<br>);
-folose($write);
-#DISPLAY COMMENTS#
-$read=fopen("com.txt","r+t")
-echo"All comments:;
-while (feof($read)){
-echo fread($read,1024);
-}
-
-fclose($read);
-
-)
-
-else(
-  $read=fopen("com.txt","r+t")
-  echo"All comments:<br>";
-
-  while (feof($read)){
-  echo fread($read,1024);
-  }
-
-  fclose($read);
-)
-
-  ?>
